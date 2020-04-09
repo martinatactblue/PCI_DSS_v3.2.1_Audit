689bc556528772be8239b95d062f76e0f56f3fa6
Martin Middleton <mmiddleton@actbluetech.com>
2020-04-09 16:42:27 -0400

    Fixes typo in requirement list

--- package.sh
+++ package.sh
@@ -3,7 +3,8 @@
 version=0.2.0
 # Make sure all the scripts are executable
 chmod 755 pci_audit.sh
-for requirement in "3 6 8"; do
+tar cvf pci_audit_${version}.tar pci_audit.sh helpers.sh
+for requirement in 3 6 8; do
     find Req_$requirement -name "*.sh" | xargs chmod 755
+    tar rvf pci_audit_${version}.tar Req_$requirement
 done
-tar cvf pci_audit_${version}.tar pci_audit.sh helpers.sh Req_3 Req_6 Req_8
